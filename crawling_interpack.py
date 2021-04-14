4f86de9ca13f4cffbb88b992fd16a052b43c4dd4
shimpark <shimpark@gmail.com>
2021-04-14 12:37:43 +0900

    interpark

---
+++ crawling_interpack.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def parsing_interpark_beautifulsoup(url):
+    """
+    뷰티풀 수프로 파싱하는 함수
+    :param url: paring할 URL. 여기선 YES24 Link
+    :return: BeautifulSoup soup Object
+    """
+
+    data = requests.get(url)
+
+    html = data.text
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
+
+
+def extract_interpark_book_data(soup):
+    """
+    BeautifulSoup Object에서 book data를 추출하는 함수
+    :param soup: BeautifulSoup soup Object
+    :return: contents(str)
+    """
+
+    upload_contents = ''
+    new_books = soup.select(".displayWrap")
+    url_prefix = "http://book.interpark.com"
+
+    for new_book in new_books:
+        book_name = new_book.select("div.infoWrap")[0].select(
+            "p.inc_tit")[0].select("b")[0].text
+        url_suffix = new_book.select("div.infoWrap")[0].select(
+            "p.inc_tit")[0].select("a")[0].attrs['href']
+        url = url_prefix + url_suffix
+        price = new_book.select("div.infoWrap")[0].select(
+            "p.inc_price")[0].select("span:nth-child(3)")[0].select("b")[0].text
+
+        content = f"<a href={url}>" + book_name + \
+            "</a>" + ", " + price + "<br/>\n"
+        upload_contents += content
+
+    return upload_contents
--- main.py
+++ main.py
@@ -2,24 +2,34 @@ import os
 from datetime import datetime
 from pytz import timezone
 from crawling_yes24 import parsing_beautifulsoup, extract_book_data
+from crawling_interpack import parsing_interpark_beautifulsoup, extract_interpark_book_data
 from github_utils import get_github_repo, upload_github_issue
 
 
 if __name__ == "__main__":
-
-    access_token = os.environ['MY_GITHUB_TOKEN']
-    repository_name = "git-action-python"
 
     seoul_timezone = timezone('Asia/Seoul')
     today = datetime.now(seoul_timezone)
     today_data = today.strftime("%Y년 %m월 %d일")
 
     yes24_it_new_product_url = "http://www.yes24.com/24/Category/NewProductList/001001003?sumGb=01"
+    interpark_it_new_product_url = "http://book.interpark.com/display/DisplayNewBook.do?_method=newBookList&sc.shopNo=0000400000&sc.dispNo=028023&bid1=NewBook&bid2=Category&bid3=Kor_Book&bid4=028023"
 
     soup = parsing_beautifulsoup(yes24_it_new_product_url)
-
     issue_title = f"YES24 IT 신간 도서 알림({today_data})"
     upload_contents = extract_book_data(soup)
+
+    soup_interpark = parsing_interpark_beautifulsoup(
+        interpark_it_new_product_url)
+    issue_interpark_title = f"인터파크 IT 신간 도서 알림({today_data})"
+    upload_interpark_contents = extract_interpark_book_data(soup_interpark)
+
+    access_token = os.environ['MY_GITHUB_TOKEN']
+    repository_name = "git-action-python"
+
     repo = get_github_repo(access_token, repository_name)
     upload_github_issue(repo, issue_title, upload_contents)
+
+    upload_github_issue(repo, issue_interpark_title, upload_interpark_contents)
+
     print("Upload Github Issue Success!")
