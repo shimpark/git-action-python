80c83085982984542502ccb6a655cb292b90f1d9
shimpark <shimpark@gmail.com>
2021-04-26 18:18:57 +0900

    챗봇 테스트

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-# hello.py
-print("Hello world")
+import telegram
+
+TELEGRAM_TOKEN = "1704418208:AAFj7~~"
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+updates = bot.getUpdates()
+chat_id = updates[-1].message.chat_id
+
+bot.sendMessage(chat_id=chat_id, text='메시지 발송')
+
+for update in updates:
+    print(update.message)
+
+    
