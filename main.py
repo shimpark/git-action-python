21c793fea201cd72b2add4d0b1a0c9fb9b04d6b4
재우니 <shimpark@gmail.com>
2024-09-04 00:19:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     today = datetime.now(seoul_timezone)
     today_data = today.strftime("%Y년 %m월 %d일")
 
-    yes24_it_new_product_url = "http://www.yes24.com/24/Category/NewProductList/001001003?sumGb=01"
+    yes24_it_new_product_url = "https://www.yes24.com/Product/Category/NewProduct?categoryNumber=001001003&pageNumber=1&pageSize=120&newProductType=NEW"
     interpark_it_new_product_url = "http://book.interpark.com/display/DisplayNewBook.do?_method=newBookList&sc.shopNo=0000400000&sc.dispNo=028023&bid1=NewBook&bid2=Category&bid3=Kor_Book&bid4=028023"
     # kyobo_it_new_product_url = "https://product.kyobobook.co.kr/category/KOR/3315"
     
@@ -27,13 +27,14 @@ if __name__ == "__main__":
     issue_title = f"YES24 IT 신간 도서 알림({today_data})"
     upload_contents = extract_book_data(soup)
     
+    # print(upload_contents)
     
     soup_interpark = parsing_interpark_beautifulsoup(
         interpark_it_new_product_url)
     issue_interpark_title = f"인터파크 IT 신간 도서 알림({today_data})"
     upload_interpark_contents = extract_interpark_book_data(soup_interpark, 'https://book.interpark.com')
 
-    print(upload_interpark_contents)
+    # print(upload_interpark_contents)
 
 
     access_token = os.environ['MY_GITHUB_TOKEN']
